700bfd7fc1bf47410e584cf64abc9033337f21ab
sergi001 <ambrosemusyoka47@gmail.com>
2020-12-30 08:18:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//creating the themes function
+document.getElementById("theme").addEventListener('click',
+    function(){
+        var but = document.querySelector('.container');
+        but.classList.toggle("containerLight");
+        but.classList.toggle()
+    });
